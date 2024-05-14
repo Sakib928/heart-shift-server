621507232179727128cffc15dab911796ffaa7a6
Sakib928 <sakibgscian@gmail.com>
2024-05-14 15:49:05 +0600

    fixed some query related bugs

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middlewares
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'a11-client-b9a9f.web.app'],
+    credentials: true
+}))
 app.use(express.json());
 
 
@@ -25,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const productCollection = client.db("productsDB").collection("boycotts");
         const recommendCollection = client.db("productsDB").collection("recommendations");
 
@@ -129,8 +132,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
